a1721b24bfa8f603705adc4ed835e7c105533514
rafiqhassani <rafiqhassani106@gmail.com>
2026-02-26 12:24:02 +0500

    Updated UI behavior

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+const todos = [];
+
 const input = document.getElementById("taskInput");
 const btn = document.getElementById("addTaskBtn");
-const display = document.getElementById("display");
+const list = document.getElementById("todo-list");
 
-btn.addEventListener("click", () => {
-  display.textContent = input.value;
+btn.addEventListener("click", function () {
+  if (input.value.trim() === "") return;
+
+  const todo = {
+    id: Date.now(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  todos.push(todo);
+
+  renderTodos();
   input.value = "";
 });
+
+function renderTodos() {
+  list.innerHTML = "";
+  todos.forEach(function (todo) {
+    const li = document.createElement("li");
+    li.textContent = `${todo.title} (${todo.createdAt.toLocaleString()})`;
+
+    if (todo.completed) {
+      li.style.textDecoration = "line-through";
+    }
+
+    li.addEventListener("click", function () {
+      todo.completed = !todo.completed;
+      renderTodos();
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function (e) {
+      e.stopPropagation();
+
+      const index = todos.findIndex((t) => t.id === todo.id);
+      todos.splice(index, 1);
+      renderTodos();
+    });
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+}
